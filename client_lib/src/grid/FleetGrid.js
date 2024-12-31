ef7739d38467ee0e5efc3f5122c5446dd18e2531
atari-monk <atari.monk1@gmail.com>
2024-12-31 16:44:29 +0100

    refactor(fleet Grid): refactor Fleet Grid for readability

--- client_lib/src/grid/FleetGrid.js
+++ client_lib/src/grid/FleetGrid.js
@@ -3,73 +3,113 @@ export class FleetGrid {
     this.dataService = dataService
     this.playerID = playerID
     this.fleetPlacement = fleetPlacement
-    this.container = null
     this.grid = grid
+    this.container = null
   }
 
   init(container) {
     this.container = container
     this.container.innerHTML = ''
-
     this.renderGrid()
     this.renderControls()
   }
 
   renderGrid() {
-    const gridState = this.dataService.getPlayerGrid(this.playerID)
-
+    const gridState = this.getPlayerGridState()
     this.grid.init(
       gridState,
-      (x, y) =>
-        this.fleetPlacement.previewPlacement(x, y, (x, y) =>
-          this.grid.getCellElement(x, y)
-        ),
-      (x, y) =>
-        this.fleetPlacement.clearPreview(x, y, (x, y) =>
-          this.grid.getCellElement(x, y)
-        ),
-      (x, y) =>
-        this.fleetPlacement.placeShip(
-          x,
-          y,
-          () => this.renderGrid(),
-          () => this.renderControls(),
-          (message) => console.log(message)
-        ),
-      (cellStatus) => {
-        switch (cellStatus) {
-          case 'ship':
-            return 'blue'
-          case 'hit':
-            return 'red'
-          case 'miss':
-            return 'gray'
-          default:
-            return 'white'
-        }
-      }
+      this.getPreviewPlacementHandler(),
+      this.getClearPreviewHandler(),
+      this.getPlaceShipHandler(),
+      this.getCellColorHandler()
     )
   }
 
+  getPlayerGridState() {
+    return this.dataService.getPlayerGrid(this.playerID)
+  }
+
+  getPreviewPlacementHandler() {
+    return (x, y) =>
+      this.fleetPlacement.previewPlacement(x, y, this.getCellElementHandler())
+  }
+
+  getClearPreviewHandler() {
+    return (x, y) =>
+      this.fleetPlacement.clearPreview(x, y, this.getCellElementHandler())
+  }
+
+  getPlaceShipHandler() {
+    return (x, y) =>
+      this.fleetPlacement.placeShip(
+        x,
+        y,
+        this.renderGrid.bind(this),
+        this.renderControls.bind(this),
+        this.logMessage.bind(this)
+      )
+  }
+
+  getCellColorHandler() {
+    return (cellStatus) => this.getCellColor(cellStatus)
+  }
+
+  getCellElementHandler() {
+    return (x, y) => this.grid.getCellElement(x, y)
+  }
+
+  getCellColor(cellStatus) {
+    switch (cellStatus) {
+      case 'ship':
+        return 'blue'
+      case 'hit':
+        return 'red'
+      case 'miss':
+        return 'gray'
+      default:
+        return 'white'
+    }
+  }
+
   renderControls() {
+    const controlsElement = this.createControlsElement()
+    this.container.appendChild(controlsElement)
+  }
+
+  createControlsElement() {
     const controlsElement = document.createElement('div')
     controlsElement.classList.add('battleship-controls')
 
-    const directionButton = document.createElement('button')
-    directionButton.textContent = `Direction: ${this.fleetPlacement.placementDirection}`
-    directionButton.addEventListener('click', () => {
-      this.fleetPlacement.togglePlacementDirection()
-      directionButton.textContent = `Direction: ${this.fleetPlacement.placementDirection}`
-    })
-
-    const infoElement = document.createElement('p')
-    infoElement.textContent = `Place ship of size: ${
-      this.fleetPlacement.fleet[this.fleetPlacement.currentShipIndex]
-    }`
+    const directionButton = this.createDirectionButton()
+    const infoElement = this.createInfoElement()
 
     controlsElement.appendChild(directionButton)
     controlsElement.appendChild(infoElement)
 
-    this.container.appendChild(controlsElement)
+    return controlsElement
+  }
+
+  createDirectionButton() {
+    const button = document.createElement('button')
+    button.textContent = `Direction: ${this.fleetPlacement.placementDirection}`
+    button.addEventListener('click', () => this.toggleDirection(button))
+    return button
+  }
+
+  toggleDirection(button) {
+    this.fleetPlacement.togglePlacementDirection()
+    button.textContent = `Direction: ${this.fleetPlacement.placementDirection}`
+  }
+
+  createInfoElement() {
+    const infoElement = document.createElement('p')
+    const currentShipSize =
+      this.fleetPlacement.fleet[this.fleetPlacement.currentShipIndex]
+    infoElement.textContent = `Place ship of size: ${currentShipSize}`
+    return infoElement
+  }
+
+  logMessage(message) {
+    console.log(message)
   }
 }
